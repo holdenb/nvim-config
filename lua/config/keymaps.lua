69ad1484bb5ab13a206cea7ffd93817b438cecce
Holden Babineaux <hbabineaux@seegrid.com>
2025-06-10 09:38:26 -0500

    Remove comments

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Global keymaps --
 
 -- Insert mode escape
 vim.keymap.set("i", "jk", "<Esc>")
@@ -9,6 +7,20 @@ vim.keymap.set("i", "kj", "<Esc>")
 -- Add empty lines before and after cursor line
 vim.keymap.set("n", "gO", "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>")
 vim.keymap.set("n", "go", "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>")
+
+-- Allow Tab to skip past specific closing indicators
+vim.keymap.set("i", "<Tab>", function()
+  local col = vim.fn.col(".")
+  local next_char = vim.fn.getline("."):sub(col, col)
+  if vim.tbl_contains({ ")", "]", "}", "'", '"', "`" }, next_char) then
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Right>", true, false, true), "n", true)
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
+  end
+end, { expr = true })
+
+------------------------------------------------------------------
+-- Neovim keymaps --
 
 -- Debugging keybinds
 if not vim.g.vscode then
@@ -40,23 +52,6 @@ if not vim.g.vscode then
   vim.keymap.set("n", "<Leader>dx", function()
     dap.terminate()
   end, { desc = "Stop Debugging" })
-
-  vim.keymap.set("n", "<leader>mi", ":MoltenInit<CR>", { desc = "Molten Init Kernel" })
-  vim.keymap.set("n", "<leader>ml", ":MoltenEvaluateLine<CR>", { desc = "Molten Eval Line" })
-  vim.keymap.set("v", "<leader>mv", ":MoltenEvaluateVisual<CR>", { desc = "Molten Eval Visual" })
-  vim.keymap.set("n", "<leader>mo", ":MoltenShowOutput<CR>", { desc = "Molten Show Output" })
-
-  vim.keymap.set("n", "<leader>mr", function()
-    require("notebook").run_cell()
-  end, { desc = "Run Current Cell" })
-
-  vim.keymap.set("n", "<leader>mn", function()
-    require("notebook").next_cell()
-  end, { desc = "Move to Next Cell" })
-
-  vim.keymap.set("n", "<leader>mp", function()
-    require("notebook").prev_cell()
-  end, { desc = "Move to Previous Cell" })
 else
 end
 
@@ -68,16 +63,8 @@ end
 --   dapui.toggle()
 -- end, { desc = "Toggle Debug UI" })
 
--- Allow Tab to skip past specific closing indicators
-vim.keymap.set("i", "<Tab>", function()
-  local col = vim.fn.col(".")
-  local next_char = vim.fn.getline("."):sub(col, col)
-  if vim.tbl_contains({ ")", "]", "}", "'", '"', "`" }, next_char) then
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Right>", true, false, true), "n", true)
-  else
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
-  end
-end, { expr = true })
+------------------------------------------------------------------
+-- VSCode keymaps --
 
 if vim.g.vscode then
   vim.keymap.set("n", "<leader>ca", function()
